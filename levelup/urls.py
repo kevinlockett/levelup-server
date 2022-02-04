6f2a88e0767aa6beae61e96783a246a62a804656
Kevin Lockett <chlockett@msn.com>
2022-02-04 13:35:53 -0600

    Compoleted Chapter 6

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from levelupapi.views import register_user, login_user
+from rest_framework import routers
+from levelupapi.views import register_user, login_user, EventView, GameView, GameTypeView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'events', EventView, 'event')
+router.register(r'games', GameView, 'game')
+router.register(r'gametypes', GameTypeView, 'gametype')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
